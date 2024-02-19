c0697778002d7b1d308489d9b3b09db9d2a052a4
emilianogastaldo <emiliano0303@gmail.com>
2024-02-19 15:02:39 +0100

    snack 1 completed

--- snack1.php
+++ snack1.php
@@ -7,59 +7,63 @@ Stampiamo a schermo tutte le partite con questo schema:
 
 Olimpia Milano - Cantù | 55-60
 */ 
-echo 'Ciao';
 
 //Creo l'array di partenza
 $matches = [
     [
-        $home_team = [
-            'name' => 'Olimpia Milano',
-            'score' => 55
-        ],
-        $guest_team =[
-            'name' => 'Cantù',
-            'score' => 60
-        ]
-        ],
+        'home_team_name' => 'Olimpia Milano',
+        'home_team_score' => 55,
+        'guest_team_name' => 'Cantù',
+        'guest_team_score' => 60
+    ],
     [
-        $home_team = [
-            'name' => 'Varese Roosters',
-            'score' => rand(1,100)
-        ],
-        $guest_team =[
-            'name' => 'Roseto Sharks',
-            'score' => rand(1,100)
-        ]
-        ],
+        'home_team_name' => 'Varese Roosters',
+        'home_team_score' => rand(1,100),
+        'guest_team_name' => 'Roseto Sharks',
+        'guest_team_score' => rand(1,100)
+    ],
     [
-        $home_team = [
-            'name' => 'Rimini Crabs',
-            'score' => rand(1,100)
-        ],
-        $guest_team =[
-            'name' => 'Legnano Knights',
-            'score' => rand(1,100)
-        ]
-        ],
+        'home_team_name' => 'Rimini Crabs',
+        'home_team_score' => rand(1,100),
+        'guest_team_name' => 'Legnano Knights',
+        'guest_team_score' => rand(1,100)
+    ],
     [
-        $home_team = [
-            'name' => 'Bologna Eagles',
-            'score' => rand(1,100)
-        ],
-        $guest_team =[
-            'name' => 'Draghi Novara',
-            'score' => rand(1,100)
-        ]
-        ],
+        'home_team_name' => 'Bologna Eagles',
+        'home_team_score' => rand(1,100),        
+        'guest_team_name' => 'Draghi Novara',
+        'guest_team_score' => rand(1,100)
+        
+    ],
     [
-        $home_team = [
-            'name' => 'Cantù',
-            'score' => rand(1,100)
-        ],
-        $guest_team =[
-            'name' => 'Roseto Sharks',
-            'score' => rand(1,100)
-        ]
-        ],
+        'home_team_name' => 'Cantù',
+        'home_team_score' => rand(1,100),        
+        'guest_team_name' => 'Roseto Sharks',
+        'guest_team_score' => rand(1,100)        
+    ],
 ]
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <section>
+        <ul>
+            <?php foreach($matches as $index => $match) : ?>
+            <li>
+                <h4> Match <?= $index + 1 ?>: </h4>
+                <!-- Olimpia Milano - Cantù | 55-60 -->
+                <div>
+                    <?= "{$match['home_team_name']} - {$match['guest_team_name']} | {$match['home_team_score']}-{$match['guest_team_score']}"?>                                         
+                </div>
+            </li>
+            <?php endforeach ?>
+        </ul>
+    </section>
+</body>
+</html>
